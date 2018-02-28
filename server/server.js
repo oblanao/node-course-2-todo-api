a6abc23abea414bb8e481d076de9e6ed08473040
Bogdan Posedaru <bogdan.posedaru@gmail.com>
2018-02-28 11:33:55 +0200

    Changed error status for adding todos

--- server/server.js
+++ server/server.js
@@ -13,11 +13,11 @@ app.post('/todos', (req, res) => {
     var todo = new Todo({
         text: req.body.text
     });
-
+    
     todo.save().then((doc) => {
         res.send(JSON.stringify(doc, null, 2));
     }, (err) => {
-        res.status(400).send('Unable to save todo.');
+        res.status(400).send(err);
     })
 })
 
