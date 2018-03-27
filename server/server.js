523d87fa981f2b4bf88339c7526044cf1d06c974
Bogdan Posedaru <bogdan.posedaru@gmail.com>
2018-03-27 12:33:51 +0300

    Added token methods

--- server/server.js
+++ server/server.js
@@ -96,10 +96,15 @@ app.patch('/todos/:id', (req, res) => {
 app.post('/users', (req, res) => {
     var body = _.pick(req.body, ['email', 'password']);
     var user = new User(body);
-    
-    user.save().then((doc) => {
-        res.send(JSON.stringify(doc, null, 2))
-    }, (err) => {
+
+    user.save()
+    .then(() => {
+        return user.generateAuthToken();    
+    })
+    .then((token) => {
+        res.header('x-auth', token).send(user);
+    })
+    .catch((err) => {
         res.status(400).send(err);
     })
 })
